c726806d3829ea0d382dcc4fd201b206193abd8c
PraveenAnil <praveendosth@gmail.com>
2021-10-14 22:11:45 +0530

    Update script.sh

--- setup/azure/scripts/script.sh
+++ setup/azure/scripts/script.sh
@@ -8,13 +8,10 @@ sudo apt update -y
 # Install the dependencies 
 sudo apt install ant gcc g++ libkrb5-dev *libmysqlclient-dev libssl-dev libsasl2-dev libsasl2-modules-gssapi-mit libsqlite3-dev libtidy*dev libxml2-dev libxslt-dev maven libldap2-dev python-dev python-setuptools git -y
 sudo apt update -y
-
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
-
- curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
-     echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-     sudo apt-get update && sudo apt-get install yarn
-
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
 sudo apt-get install -y nodejs
 sudo apt update -y
 npm install --global npm

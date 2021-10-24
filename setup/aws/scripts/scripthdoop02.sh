5d915159ff6bbbb00c1cee7d3d85f1cb73ce6ba6
PraveenAnil <praveendosth@gmail.com>
2021-10-24 17:20:21 +0530

    Update scripthdoop02.sh

--- setup/aws/scripts/scripthdoop02.sh
+++ setup/aws/scripts/scripthdoop02.sh
@@ -3,6 +3,7 @@
 cd /home/hdoop
 source .bashrc
 echo -e 'yes' | ssh localhost
+exit
 echo -e 'Y' | hdfs namenode -format
 /home/hdoop/hadoop-3.2.2/sbin/start-dfs.sh
 /home/hdoop/hadoop-3.2.2/sbin/start-yarn.sh

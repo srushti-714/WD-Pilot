e9d69222f4334b8f08e447c4f2ef449522cfad52
PraveenAnil <praveendosth@gmail.com>
2021-11-09 20:20:35 +0530

    Update installhue.sh

--- setup/aws/scripts/installhue.sh
+++ setup/aws/scripts/installhue.sh
@@ -39,5 +39,7 @@ chown -R hue:hdoop /usr/share/hue
 make apps 2> /home/ubuntu/tmplog006.txt
 sed -i 's/## webhdfs_url=http:/webhdfs_url=http:/' /usr/share/hue/desktop/conf/pseudo-distributed.ini
 sed -i 's/localhost:50070/localhost:9870/' /usr/share/hue/desktop/conf/pseudo-distributed.ini
+sed -i 's/## default_hdfs_superuser=hdfs/default_hdfs_superuser=hdoop/' /usr/share/hue/desktop/conf/pseudo-distributed.ini
+
 # Run the hue server in the background and make it available on port 8000
 ./build/env/bin/hue runserver 0.0.0.0:8000 &  2> tmplog007.txt

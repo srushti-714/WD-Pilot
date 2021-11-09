46c0dfdba0763d466bae42a763ab6343816607cb
PraveenAnil <praveendosth@gmail.com>
2021-11-09 16:16:12 +0530

    Update script.sh

--- setup/aws/scripts/script.sh
+++ setup/aws/scripts/script.sh
@@ -8,7 +8,8 @@ sudo apt install openssh-server openssh-client -y
 sudo echo -e 'Password.1!!\nPassword.1!!' | adduser hdoop
 
 cd /home/hdoop
-wget https://dlcdn.apache.org/hadoop/common/hadoop-3.2.2/hadoop-3.2.2.tar.gz -P /home/hdoop 
+wget https://dlcdn.apache.org/hadoop/common/hadoop-3.2.2/hadoop-3.2.2.tar.gz -P /home/hdoop
+wget https://downloads.apache.org/hive/hive-3.1.2/apache-hive-3.1.2-bin.tar.gz -P /home/hdoop
 
 mkdir tmpdata
 mkdir /home/hdoop/dfsdata/namenode -p

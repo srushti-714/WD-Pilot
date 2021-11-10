5460ce72ddb5a3e0287a70d3926cc8a9b8bdf112
PraveenAnil <praveendosth@gmail.com>
2021-11-10 18:10:22 +0530

    Update installhue.sh

--- setup/aws/scripts/installhue.sh
+++ setup/aws/scripts/installhue.sh
@@ -43,9 +43,4 @@ sed -i 's/## default_hdfs_superuser=hdfs/default_hdfs_superuser=hdoop/' /usr/sha
 
 # Run the hue server in the background and make it available on port 8000
 ./build/env/bin/hue runserver 0.0.0.0:8000 &  2> /home/ubuntu/tmplog007.txt
-export LATEST_VER="8.10.1"
-cd
-curl -O https://downloads.apache.org/lucene/solr/${LATEST_VER}/solr-${LATEST_VER}.tgz
-tar xvf solr-${LATEST_VER}.tgz
-cd solr-${LATEST_VER}/bin/
-sudo ./install_solr_service.sh ~/solr-${LATEST_VER}.tgz & 2 > /home/ubuntu/tmplog008.txt
+

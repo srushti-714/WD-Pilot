32c464cc2696482577af9ca02426c844711ab4fd
PraveenAnil <praveendosth@gmail.com>
2021-11-14 14:38:35 +0530

    Update installsolr.sh

--- setup/aws/scripts/installsolr.sh
+++ setup/aws/scripts/installsolr.sh
@@ -4,10 +4,10 @@ cd /usr/share/hue
 curl -O https://downloads.apache.org/lucene/solr/${LATEST_VER}/solr-${LATEST_VER}.tgz
 tar xvf solr-${LATEST_VER}.tgz
 cd solr-${LATEST_VER}/bin/
-sudo ./install_solr_service.sh /usr/share/hue/solr-${LATEST_VER}.tgz &
+nohup ./install_solr_service.sh /usr/share/hue/solr-${LATEST_VER}.tgz  > /dev/null 2>&1 &
+
 
 # Run the hue server in the background and make it available on port 8000
 cd /usr/share/hue
 chown -R hue:hdoop /usr/share/hue
-
-./build/env/bin/hue runserver 0.0.0.0:8000 &  2> /home/ubuntu/tmplog007.txt
+nohup ./build/env/bin/hue runserver 0.0.0.0:8000   > /dev/null 2>/home/ubuntu/tmplog007.txt &

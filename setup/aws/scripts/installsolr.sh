b9f159c6d74da62dc8a531c9f7c71a0ceeb96a72
PraveenAnil <praveendosth@gmail.com>
2021-11-24 14:09:47 +0530

    Update installsolr.sh

--- setup/aws/scripts/installsolr.sh
+++ setup/aws/scripts/installsolr.sh
@@ -31,13 +31,13 @@ nohup ./install_solr_service.sh /usr/share/hue/solr-${LATEST_VER}.tgz  > /dev/nu
 # Run the hue server in the background and make it available on port 8000
 cd /usr/share/hue
 chown -R hue:hdoop /usr/share/hue
-build/env/bin/pip install git+https://github.com/gethue/PyHive
-build/env/bin/pip install thrift-sasl
-build/env/bin/pip install pyodbc
+#build/env/bin/pip install git+https://github.com/gethue/PyHive
+#build/env/bin/pip install thrift-sasl
+#build/env/bin/pip install pyodbc
 #build/env/bin/pip install pymssql
-build/env/bin/pip install PyAthena
-build/env/bin/pip install snowflake-sqlalchemy
-build/env/bin/pip install sqlalchemy-solr
+#build/env/bin/pip install PyAthena
+#build/env/bin/pip install snowflake-sqlalchemy
+#build/env/bin/pip install sqlalchemy-solr
 nohup ./build/env/bin/hue runserver 0.0.0.0:8000   > /dev/null 2>/home/ubuntu/tmplog007.txt &
 source ./build/env/bin/activate
 build/env/bin/hue syncdb --noinput

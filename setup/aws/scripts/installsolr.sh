6c3680e9c6a786ce48ee9d67cd69617f21b1a9b0
PraveenAnil <praveendosth@gmail.com>
2021-11-24 12:28:50 +0530

    Update installsolr.sh

--- setup/aws/scripts/installsolr.sh
+++ setup/aws/scripts/installsolr.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+
+export HADOOP_HOME=/home/hdoop/hadoop-3.2.2
+export HADOOP_INSTALL=$HADOOP_HOME
+export HADOOP_MAPRED_HOME=$HADOOP_HOME
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+export YARN_HOME=$HADOOP_HOME
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
+export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/native"
+export HIVE_HOME="/home/hdoop/apache-hive-3.1.2-bin"
+export PATH=$PATH:$HIVE_HOME/bin
+export OOZIE_HOME=/home/hdoop/oozie-5.2.1/distro/target/oozie-5.2.1-distro/oozie-5.2.1
+export HBASE_HOME=/home/hdoop/hbase
+export PATH=$PATH:$HBASE_HOME/bin
+export SPARK_HOME=/home/hdoop/spark
+export PIG_HOME=/home/hdoop/pig
+export PATH=$PATH:$PIG_HOME/bin
+export PIG_CLASSPATH=$HADOOP_HOME/etc/hadoop
+
 export LATEST_VER="8.11.0"
 cd /usr/share/hue
 curl -O https://downloads.apache.org/lucene/solr/${LATEST_VER}/solr-${LATEST_VER}.tgz
